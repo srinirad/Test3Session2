ba921a1488c8f95a62f988f41699bbe1b12709a7
Srinivasan Lakshminarayanan <srinirad@users.noreply.github.com>
2022-03-03 08:48:32 +0000

    gcd done

--- p7final.c
+++ p7final.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+struct _fraction
+{
+  int num,den;
+};
+typedef struct _fraction Fraction;
+  int gcd(int a,int b)
+  {
+    int t;
+    while (b != 0)
+      {
+        t = b;
+        b = a%b;
+        a = t;
+      }
+    return a;
+}
+int main()
+{
+  printf("%d\n",gcd(16,24));
+}
